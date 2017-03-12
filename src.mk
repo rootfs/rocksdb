b77f063f0266a806b81f699b82aacc8ecf12e9b3
Huamin Chen <hchen@redhat.com>
2017-03-12 11:10:32 -0400

    add librados to lib src
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- src.mk
+++ src.mk
@@ -185,6 +185,7 @@ LIB_SOURCES =                                                   \
   utilities/date_tiered/date_tiered_db_impl.cc                  \
   utilities/write_batch_with_index/write_batch_with_index.cc    \
   utilities/write_batch_with_index/write_batch_with_index_internal.cc    \
+  utilities/env_librados.cc                                     \
 
 TOOL_LIB_SOURCES = \
   tools/ldb_cmd.cc                                               \
